ae9b917ab039cfbdf4f8de81db780aca571e554e
Josh Allen <joshua.allen@mnsu.edu>
2017-10-04 21:02:45 +0000

    10-4-4:02

--- CoffeeTest.java
+++ CoffeeTest.java
@@ -1,7 +1,9 @@
-import org.junit.Assert.assertEquals;
-import org.junit.Assert.assertTrue;
-import org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 //import org.junit;
 
 
